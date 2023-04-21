0578956c387503ad619b02562a01f1f632ee9b79
Narayana_Reddy <narayanachavva@gmail.com>
2023-04-21 14:07:01 +0530

    client side

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./Components/Signup";
+import Login from "./Components/Login";
+import TODO from "./Components/TODO";
+import History from "./Components/History";
+import "./App.css";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/todosList" element={<TODO />} />
+        <Route path="/todosHistory" element={<History />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
